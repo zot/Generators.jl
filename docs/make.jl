bb6f1239869c2577fa3680d30cfb74d1ed6f6230
Bill Burdick <bill.burdick@gmail.com>
2022-03-11 15:24:30 +0400

    adding error in docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,6 @@
 using Documenter, Generators
 
+throw("Error")
+
 DocMeta.setdocmeta!(Generators, :DocTestSetup, :(using Generators), recursive = true)
 makedocs(sitename="fred.com", modules=[Generators], format = Documenter.HTML())
